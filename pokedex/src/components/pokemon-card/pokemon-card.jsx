95a5ee1fd695f0fc5c4bb463f1d615b851c14891
Iván Sánchez Pérez <ivan.sanchez@sugerendo.com>
2022-07-07 23:31:54 +0200

    Creo los filtros e implemento lasfunciones para recoger los valores

--- pokedex/src/components/pokemon-card/pokemon-card.jsx
+++ pokedex/src/components/pokemon-card/pokemon-card.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+
 function PokemonCard() {
+  /**
+   * Tengo que crear un useEfect para realizar el fetch a la API
+   * cuando tengo los datos, los guardo en una variable de estado
+   * que almacenará todos los valores de los pokemon.
+   * Antes de guardar la información debo ordenar los pokemon por ID
+   * función sort del array
+   *
+   */
+
+  useEffect(() => {}, []);
+
   return <h1>POKEMON CARD</h1>;
 }
 
